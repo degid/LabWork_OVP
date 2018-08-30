2b5b1492eef020fd64f79dcee51388fbee166742
degid <sdegid@gmail.com>
2018-08-30 16:34:08 +0300

    Update testswap.asm

--- testswap.asm
+++ testswap.asm
@@ -32,25 +32,51 @@ main:
  mov %ecx,%edi
  mov %rax,-0x8(%rbp)
  callq 400500 <_Znam@plt>
- xor %ecx,%ecx
  mov -0x8(%rbp),%rdi
  mov %rax,(%rdi)
  mov -0x8(%rbp),%rax
  mov 0x18(%rax),%rax
  mov -0x8(%rbp),%rdi
  mov %rax,0x8(%rdi)
- mov -0x8(%rbp),%rax
- mov (%rax),%rax
- mov %rax,-0x10(%rbp)
- mov -0x8(%rbp),%rax
- mov 0x8(%rax),%rax
  mov -0x8(%rbp),%rdi
- mov %rax,(%rdi)
- mov -0x10(%rbp),%rax
- mov -0x8(%rbp),%rdi
- mov %rax,0x8(%rdi)
- mov %ecx,%eax
+ mov -0x8(%rbp),%rax
+ add $0x8,%rax
+ mov %rax,%rsi
+ callq 4006f0 <_ZSt4swapIPiENSt9enable_ifIXsr6__and_ISt6__not_ISt15__is_tuple_likeIT_EESt21is_move_constructibleIS4_ESt18is_move_assignableIS4_EEE5valueEvE4typeERS4_SD_>
+ xor %eax,%eax
  add $0x10,%rsp
  pop %rbp
  retq 
- nopl 0x0(%rax,%rax,1)
+ nopw 0x0(%rax,%rax,1)
+_ZSt4swapIPiENSt9enable_ifIXsr6__and_ISt6__not_ISt15__is_tuple_likeIT_EESt21is_move_constructibleIS4_ESt18is_move_assignableIS4_EEE5valueEvE4typeERS4_SD_:
+ push %rbp
+ mov %rsp,%rbp
+ sub $0x20,%rsp
+ mov %rdi,-0x8(%rbp)
+ mov %rsi,-0x10(%rbp)
+ mov -0x8(%rbp),%rdi
+ callq 400740 <_ZSt4moveIRPiEONSt16remove_referenceIT_E4typeEOS3_>
+ mov (%rax),%rax
+ mov %rax,-0x18(%rbp)
+ mov -0x10(%rbp),%rdi
+ callq 400740 <_ZSt4moveIRPiEONSt16remove_referenceIT_E4typeEOS3_>
+ mov (%rax),%rax
+ mov -0x8(%rbp),%rsi
+ mov %rax,(%rsi)
+ lea -0x18(%rbp),%rdi
+ callq 400740 <_ZSt4moveIRPiEONSt16remove_referenceIT_E4typeEOS3_>
+ mov (%rax),%rax
+ mov -0x10(%rbp),%rsi
+ mov %rax,(%rsi)
+ add $0x20,%rsp
+ pop %rbp
+ retq 
+ nopl 0x0(%rax)
+_ZSt4moveIRPiEONSt16remove_referenceIT_E4typeEOS3_:
+ push %rbp
+ mov %rsp,%rbp
+ mov %rdi,-0x8(%rbp)
+ mov -0x8(%rbp),%rax
+ pop %rbp
+ retq 
+ xchg %ax,%ax
